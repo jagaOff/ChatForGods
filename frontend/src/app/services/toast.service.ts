ad7f075147f5a08ee2a47b455866eba27e195a3e
Vlad <zinkovskyi.vladyslav@gmail.com>
2024-04-02 15:17:03 +0200

    update toast notofocation with websocket

--- frontend/src/app/services/toast.service.ts
+++ frontend/src/app/services/toast.service.ts
@@ -3,11 +3,11 @@ import {Injectable} from '@angular/core';
 interface ToastConfig {
   closeButton?: boolean;
   timeOut?: number;
-  progressBar?: boolean;
   toastClass?: string;
   positionClass?: string;
   titleClass?: string;
   messageClass?: string;
+  isClosable?: boolean;
   customIcon?: string; // SVG string
 }
 
@@ -26,23 +26,29 @@ export class ToastService {
     this.appendToastToContainer(toastElement);
   }
 
+  closeElement(toastElement: HTMLElement): void {
+    this.removeToastElement(toastElement);
+  }
+
   private createToastElement(type: string, message: string, configs: ToastConfig): HTMLElement {
     const {
       closeButton = false,
       timeOut = 5000,
-      progressBar = false,
       toastClass = 'toast',
       positionClass = 'toast-top-right',
       messageClass = 'toast-message',
+      isClosable = true,
       customIcon
     } = configs;
 
     const toastElement = document.createElement('div');
+    toastElement.id = `toast-${type}`;
     toastElement.className = `${toastClass} ${positionClass}`;
     toastElement.setAttribute('role', 'alert');
 
     const iconHtml = customIcon ? customIcon : this.getIconSvg(type);
     const iconColorClass = this.getIconColorClass(type);
+
 
     toastElement.innerHTML = `
       <div class="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800">
@@ -50,17 +56,17 @@ export class ToastService {
           ${iconHtml}
           <span class="sr-only">${type} icon</span>
         </div>
-        <div class="ms-3 mr-3 text-sm font-normal ${messageClass}">${message}  </div>
+        <div class="ms-3 mr-3 text-sm font-normal ${messageClass}">
+            ${message}
+        </div>
         ${closeButton ? this.getCloseButtonHtml(type) : ''}
       </div>
+
     `;
 
-    if (progressBar) {
-      const progressElement = document.createElement('div');
-      progressElement.className = 'toast-progress';
-      progressElement.style.animationDuration = `${timeOut}ms`;
-      toastElement.appendChild(progressElement);
-    }
+    // if (progressBar) {
+    //   this.updateProgress(toastElement, timeOut, 100);
+    // }
 
     if (closeButton) {
       const closeButtonElement = toastElement.querySelector('.toast-close-button');
@@ -71,9 +77,12 @@ export class ToastService {
       }
     }
 
-    setTimeout(() => {
-      this.removeToastElement(toastElement);
-    }, timeOut);
+    if (isClosable) {
+      setTimeout(() => {
+        this.removeToastElement(toastElement);
+      }, timeOut);
+    }
+
 
     return toastElement;
   }
@@ -100,7 +109,7 @@ export class ToastService {
       case 'success':
         return '<svg aria-hidden="true" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />  <polyline points="22 4 12 14.01 9 11.01" /></svg>';
       case 'info':
-        return '<svg aria-hidden="true" class="w-6 h-6" fill="none" viewBox="0 0 18 20" stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="12" y1="16" x2="12" y2="12" />  <line x1="12" y1="8" x2="12.01" y2="8" /></svg>';
+        return '<svg aria-hidden="true" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="12" y1="16" x2="12" y2="12" />  <line x1="12" y1="8" x2="12.01" y2="8" /></svg>';
       case 'warning':
         return '<svg aria-hidden="true" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" />  <line x1="12" y1="9" x2="12" y2="13" />  <line x1="12" y1="17" x2="12.01" y2="17" /></svg>';
       case 'error':
@@ -113,7 +122,7 @@ export class ToastService {
   private getCloseButtonHtml(type: string): string {
     return `
       <button aria-label="Close"
-              class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+              class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700 toast-close-button"
               data-dismiss-target="#toast-${type}" type="button">
         <span class="sr-only">Close</span>
         ${this.getCloseIconSvg()}
--- frontend/src/app/services/websocket.service.ts
+++ frontend/src/app/services/websocket.service.ts
@@ -18,7 +18,8 @@ export class WebsocketService implements OnDestroy {
   private subscription: StompSubscription | undefined;
   private timeOut = 5000;
   private connectionAttempts = 0;
-  private maxConnectionAttempts = 5;
+  private maxConnectionAttempts = 2;
+  private closeReconnect = false;
 
   constructor(private userConfig: UserConfig, private toast: ToastService) {
 
@@ -50,6 +51,8 @@ export class WebsocketService implements OnDestroy {
 
     });
 
+
+
     if (this.connection?.connected) {
       this.connectionAttempts = 0;
       this.connectionStatus = 'Connected';
@@ -58,21 +61,14 @@ export class WebsocketService implements OnDestroy {
         timeOut: 3000,
       });
     } else {
-      this.reconnect();
-    }
-  }
-
-  private reconnect() {
-    this.connectionAttempts++;
-    if (this.connectionAttempts < this.maxConnectionAttempts) {
-      console.log("trying to connect to websocket: " + this.connectionAttempts);
-      setTimeout(() => this.connectToWebSocket(), this.timeOut);
-    } else {
-      this.connectionStatus = 'Could not connect to websocket';
-      this.toast.show("error", 'Could not connect to websocket', {
-        closeButton: true,
-        timeOut: 3000,
-      });
+      if (!this.closeReconnect) {
+        this.closeReconnectNotification()
+        this.toast.show("info", "Trying to connect to websocket", {
+          isClosable: false,
+          customIcon: '<img src="assets/img/svg/loading.svg" alt="Star Icon" />',
+        });
+        this.reconnect();
+      }
     }
   }
 
@@ -87,7 +83,7 @@ export class WebsocketService implements OnDestroy {
         closeButton: true,
         timeOut: 3000,
       });
-      this.reconnect();
+      // this.reconnect();
     });
   }
 
@@ -100,18 +96,44 @@ export class WebsocketService implements OnDestroy {
     }
   }
 
-  /*subscribe(callback: Function) {
-    this.stompClient.subscribe('/topic/public', (message: any) => {
-      // callback(JSON.parse(message.body));
-    });
-  }*/
-
   public subscribe(destination: string, callback: Function): void {
     if (this.connection) {
       this.connection.connect({}, () => {
         this.subscription = this.connection!.subscribe(destination, message => {
           callback(JSON.parse(message.body));
         });
+      });
+    }
+  }
+
+  private closeReconnectNotification() {
+    const element = document.getElementById('toast-info');
+    if (element) {
+      console.log('Closing element')
+      this.toast.closeElement(element)
+    }
+  }
+
+  /*subscribe(callback: Function) {
+    this.stompClient.subscribe('/topic/public', (message: any) => {
+      // callback(JSON.parse(message.body));
+    });
+  }*/
+
+  private reconnect() {
+    this.connectionAttempts++;
+
+
+    if (this.connectionAttempts <= this.maxConnectionAttempts) {
+      console.log("trying to connect to websocket: " + this.connectionAttempts);
+      setTimeout(() => this.connectToWebSocket(), this.timeOut);
+    } else {
+      this.closeReconnectNotification()
+      this.closeReconnect = true;
+      this.connectionStatus = 'Could not connect to websocket';
+      this.toast.show("error", 'Could not connect to websocket', {
+        closeButton: true,
+        timeOut: 3000,
       });
     }
   }
