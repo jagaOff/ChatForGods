945be26dae52eed27d11952af2f0cefadbc5aef3
Vlad <zinkovskyi.vladyslav@gmail.com>
2024-04-07 14:01:29 +0200

    create dropdown menu

--- frontend/src/app/components/left-index/left-index.component.ts
+++ frontend/src/app/components/left-index/left-index.component.ts
@@ -1,12 +1,39 @@
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import { Component } from '@angular/core';
+import { ThemeConfig } from '../../config/Theme.config';
+import { UserConfig } from '../../config/User.config';
 
 @Component({
   selector: 'app-left-index',
   standalone: true,
   imports: [],
   templateUrl: './left-index.component.html',
-  styleUrl: './left-index.component.css'
+  styleUrl: './left-index.component.css',
+
 })
 export class LeftIndexComponent {
 
+  isMenuVisible = false;
+  isDarkTheme!: boolean ;
+
+  constructor(private themeConfig: ThemeConfig,
+              private userConfig: UserConfig){
+    this.themeConfig.getTheme().then(theme => {
+      this.isDarkTheme = theme === 'dark';
+    })
+  }
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.themeConfig.changeTheme(this.isDarkTheme ? 'dark' : 'light');
+  }
+
+  signOut() {
+    this.userConfig.signOut();
+    window.location.reload();
+  }
 }
